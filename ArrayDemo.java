a794a65065eed64306ced2a9fe6893e1f794d613
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2025-08-19 14:19:20 +0530

    Create ArrayDemo.java

---
+++ ArrayDemo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArrayDemo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Value of N:");
+		int n = sc.nextInt();
+		int [] array = new int[n];		
+		System.out.println("Enter the Elements:");
+		for (int i=0;i<n;i++) {
+			array[i] = sc.nextInt();
+		}
+		//print the array
+		System.out.println("The Given array: ");
+		for(int num:array){
+			System.out.print(num+" ");
+		}
+		System.out.println();
+		//largest element
+		int largest = array[0];
+		for(int i=0;i<n;i++) {
+			if (array[i]>largest) {
+				largest = array[i];
+				
+			}
+		}
+		System.out.println("The largest element in the array is: "+largest);
+		sc.close();
+	}
+}
