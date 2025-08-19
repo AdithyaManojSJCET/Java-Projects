d5633d888c20cbf0800c9e7261cf885513ec5841
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2025-08-19 14:37:37 +0530

    Create TwoDArray.java

---
+++ TwoDArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TwoDArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [][] matrix = {
+				{1,2,3},
+				{4,5,6},
+				{7,8}
+		};
+		for(int i=0;i<matrix.length;i++) {
+			for(int j=0;j<matrix[i].length;j++) {
+				System.out.print(matrix[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+	
+}
